e59931e7031c079d788d8dbc249f1e70dc0bab5c
devThiagoBarboza <devthiagobarboza@gmail.com>
2022-01-19 08:58:33 -0300

    sorteadores de jogos da loteria

---
+++ lotoFacil.js
@@ -0,0 +1,26 @@
+let dezenas = []
+
+//inserir no array dezenas os 25 números
+for (const x of Array(25).keys()){
+        dezenas.push(x)
+    } 
+//função para escolha de números aleatórios em um array
+    const escolherDezenas = (arr, num = 1) => {
+        const res = [];
+    
+        for(let i = 0; i < num; ){
+           const random = Math.floor(Math.random() * arr.length);
+           if(res.indexOf(arr[random]) !== -1){
+              continue;
+           };
+           res.push(arr[random]);
+           i++;
+        };
+        return res;
+     };
+
+
+var dezenasParaJogar = escolherDezenas(dezenas, 15)
+console.log(dezenasParaJogar.sort(function (a, b){
+    return a - b 
+    }));
